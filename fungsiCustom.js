5be6f39da214bed9c151dbca313d21cf84637a7e
mnaauval <mnaauval20@gmail.com>
2022-06-09 06:47:36 +0700

    feat: fungsiCustom, getLastMessage

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,29 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  fs.readFile(file1, "utf-8", (err, data1) => {
+    data1 = JSON.parse(data1);
+    data1 = getLastMessage(data1.message);
+
+    fs.readFile(file2, "utf-8", (err, data2) => {
+      data2 = JSON.parse(data2);
+      data2 = getLastMessage(data2[0].message);
+
+      fs.readFile(file3, "utf-8", (err, data3) => {
+        data3 = JSON.parse(data3);
+        data3 = getLastMessage(data3[0].data.message);
+
+        fnCallback(err, [data1, data2, data3]);
+      });
+    });
+  });
+};
+
+const getLastMessage = (data) => {
+  const arrData = data.split(" ").reverse();
+  return arrData[0];
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
